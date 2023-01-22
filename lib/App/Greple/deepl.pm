7c3d482de35bb6f20832ccb6305a584ef1f6f0ee
Kazumasa Utashiro <kaz@utashiro.com>
2023-01-22 22:03:51 +0900

    minil new App::Greple::deepl

---
+++ lib/App/Greple/deepl.pm
@@ -0,0 +1,39 @@
+package App::Greple::deepl;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Greple::deepl - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Greple::deepl;
+
+=head1 DESCRIPTION
+
+App::Greple::deepl is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
